8f5c42db87c0bcf300da9226d82a36cebd3c77ba
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-12-01 15:26:06 +0100

    Milestone 1: added components

--- index.php
+++ index.php
@@ -6,16 +6,9 @@ include_once __DIR__ . "/data/data.php";
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spotify</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<?php
+include_once __DIR__ . "/components/pageHead.php"
+?>
 
 <body>
     <header class="bg-header p-3">
